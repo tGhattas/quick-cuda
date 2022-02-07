238474c4a8e0979d2be25d9392adc378783e8f18
tGhattas <tammer.ghattas@gmail.com>
2022-02-07 12:04:37 +0200

    + quick

--- quickCUDA.cu
+++ quickCUDA.cu
@@ -27,6 +27,27 @@ void simpleBfs(int N, int level, int *d_adjacencyList, int *d_edgesOffset,
     }
 }
 
+__global__
+void quick(int N, int level, int *d_adjacencyList, int *d_edgesOffset,
+               int *d_edgesSize, int *d_distance, int *d_parent, int *changed) {
+    int thid = blockIdx.x * blockDim.x + threadIdx.x;
+    int valueChange = 0;
 
+    if (thid < N && d_distance[thid] == level) {
+        int u = thid;
+        for (int i = d_edgesOffset[u]; i < d_edgesOffset[u] + d_edgesSize[u]; i++) {
+            int v = d_adjacencyList[i];
+            if (level + 1 < d_distance[v]) {
+                d_distance[v] = level + 1;
+                d_parent[v] = i;
+                valueChange = 1;
+            }
+        }
+    }
+
+    if (valueChange) {
+        *changed = valueChange;
+    }
+}
 
 }
